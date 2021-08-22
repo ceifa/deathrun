9005d7513bc4f208a6494e61d7a52bc85262157b
BuildTools <unconfigured@null.spigotmc.org>
2021-08-22 16:53:52 -0300

    Added USE

---
+++ code/Entity/TriggerPushFix.cs
@@ -0,0 +1,22 @@
+﻿
+using Sandbox;
+
+namespace deathrun
+{
+
+	[Library( "trigger_push_fix" )]
+	[Hammer.Solid]
+	public partial class TriggerPushFix : BaseTrigger
+	{
+
+		public override void StartTouch( Entity other )
+		{
+			base.Touch( other );
+
+			if ( other.IsWorld )
+				return;
+			other.Velocity = Vector3.Backward * 1000;
+
+		}
+	}
+}
